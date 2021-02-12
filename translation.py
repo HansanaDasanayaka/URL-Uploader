106f41ed7ca458deaf810e4ef2c41303c87aa5a3
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-02-13 01:33:25 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,30 +1,28 @@
 class Translation(object):
 
-    START_TEXT = """Hello,
+    START_TEXT = """<b>Hello, This is a Telegram URL Upload Bot 🤖
 
-This is a Telegram URL Upload Bot!
+Please send me any Direct download URL link, I can upload to telegram as File or Video 🥳
 
-<b>Please send me any Direct download URL link, I can upload to telegram as File/Video</b>
+/help for more details 🙃
 
-/help for more details..
-
-Support Group : @TroJanzSupport
+Made with ❤ 🇱🇰 by @Infinity_BOTs</b>
 """
 
-    HELP_USER = """Hai I'am a URL Uploader bot..
+    HELP_USER = """<u><b>More Help and Commands</u>
     
-1. Send url (Link | New Name with Extension).
-2. Send Custom Thumbnail (Optional).
-3. Select the button.
+1. Send URL (Link | New Name with Extension)
+2. Send Custom Thumbnail (Optional)
+3. Select the button
    SVideo - Give File as video with Screenshots
    DFile  - Give File with Screenshots
    Video  - Give File as video without Screenshots
    DFile  - Give File without Screenshots
 
-Support Group : @TroJanzSupport
+Made by @Infinity_BOTs</b>
 """
 
-    FORMAT_SELECTION = """Select the desired format: <a href='{}'>file size might be approximate</a>
+    FORMAT_SELECTION = """Select the desired format, file size might be approximate.
     
 Send your custum thumbnail if required.
 You can use /deletethumbnail to delete the auto-generated thumbnail."""
@@ -35,14 +33,14 @@ URL | newfilename | username | password"""
 
     DOWNLOAD_START = "Downloading..."
     
-    UPLOAD_START = "Uploading now.."
+    UPLOAD_START = "Uploading..."
     
-    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds. \n\nUploaded in {} seconds."
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds. \n\nUploaded in {} seconds.\n\nBy @Infinity_BOTs"
 
     RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.95GB due to Telegram API limitations."
 
     CUSTOM_CAPTION_UL_FILE = " "
 
-    SLOW_URL_DECED = "Gosh that seems to be a very slow URL. Since you were screwing my home, I am in no mood to download this file. Meanwhile, why don't you try this:==> https://shrtz.me/PtsVnf6 and get me a fast URL so that I can upload to Telegram, without me slowing down for other users."
+    SLOW_URL_DECED = "Nigga that seems to be a very slow URL. Since you were screwing my home, I am in no mood to download this file. Meanwhile, why don't you try @HK_transloader_BOT and get me a fast URL so that I can upload to Telegram, without me slowing down for other users."
 
     NO_VOID_FORMAT_FOUND = "ERROR...\n<b>YouTubeDL</b> said: {}"
